d1ce915dd1cf6707846b78c9fa881e1421bf9082
Finlayson <W0506675@campus.nscc.ca>
2025-01-06 15:16:04 -0400

    Initial commit

---
+++ PropertiesandEncapsulation_06012024_SofiaFinlayson/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesandEncapsulation_06012024_SofiaFinlayson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class Character
+    { 
+        // public int characterName(string NAME)
+        // int health... 100
+        //isALive = true if health > 0 if health...   if health is 0<= player dead
+    }
+
+    public class Inventory
+    {
+
+    }
+
+    public class PowerUp
+    {
+
+    }
+}
